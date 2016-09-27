9a16f34a7b7eaaea78fb709cb998ab8c7fcf8954
FoX31 <H-ebreh@mail.ru>
2016-09-27 15:18:56 +0300

    Создание теста без использования рекордера

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GoupModificationTest.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by e.kutsenko on 27.09.2016.
+ */
+public class GoupModificationTest extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().filGroupForm(new GroupData("Fox1", "Fox2", "Fox3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
