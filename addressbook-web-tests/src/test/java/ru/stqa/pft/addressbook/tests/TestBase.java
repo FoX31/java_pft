83ae8720e9b0e07b0f38ed3d94ee65eaa7a465ad
FoX31 <H-ebreh@mail.ru>
2016-12-14 16:31:56 +0300

    Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,9 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by e.kutsenko on 23.09.2016.
@@ -23,4 +33,13 @@ public class TestBase {
         app.stop();
     }
 
+    public void verifyGroupListInUI() {
+      if (Boolean.getBoolean("verifyUI")) {
+        Groups dbGroups = app.db().groups();
+        Groups uiGroups = app.group().all();
+        assertThat(uiGroups, equalTo(dbGroups.stream()
+                .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                .collect(Collectors.toSet())));
+      }
+    }
 }
